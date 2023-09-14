1f556a1f38db7c98b20eec7ef826c16775136149
triborg34 <aref.mousavi0914@gmail.com>
2023-09-14 15:01:50 +0330

    changes

--- lib/widgets/widgetScreens/CoustomDrawer.dart
+++ lib/widgets/widgetScreens/CoustomDrawer.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: must_be_immutable
+
 import 'package:expansion_widget/expansion_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
@@ -46,47 +48,10 @@ class CoustomDrawer extends StatelessWidget {
               ),
             ),
             // CollapeWidget(),
-            ExpansionWidget(
-
-                initiallyExpanded: true,
-                titleBuilder:
-                    (double animationValue, _, bool isExpaned, toogleFunction) {
-                  return InkWell(
-                      onTap: () => toogleFunction(animated: true),
-                      child: Container(
-                      padding: EdgeInsets.only(right: 10),
-                        color: lightGreen,
-                        child: Row(
-                          crossAxisAlignment: CrossAxisAlignment.center,
-                          children: [
-                           
-                            Transform.rotate(
-                              angle: math.pi * animationValue / 2,
-                              child: Icon(
-                                Icons.arrow_right,
-                                size: 40,
-                                color: Colors.white,
-                              ),
-                              alignment: Alignment.center,
-                            ),
-                             Expanded(
-                                child: Text(
-                              'فیلم',
-                              textAlign: TextAlign.right,
-                              style: TextStyle(color: white,fontSize: 18,fontWeight: FontWeight.bold),
-                            )),
-                          ],
-                        ),
-                      ));
-                },
-                content: Container(
-                  width: double.infinity,
-                  color: darkGreen,
-                  padding: EdgeInsets.all(20),
-                  child: Column(children: [
-                    
-                  ],)
-                )),
+            ExpansionPanels(header: "فیلم",contant: Container(child: Text("data"),)),
+            ExpansionPanels(header: "سریال",contant: Container(child: Text("data"),)),
+            ExpansionPanels(header: "انیمیشن",contant: Container(child: Text("data"),)),
+            ExpansionPanels(header: "انیمه",contant: Container(child: Text("data"),)),
             Container(
                 color: Colors.red,
                 child: ListTile(
@@ -107,5 +72,49 @@ class CoustomDrawer extends StatelessWidget {
           ],
         ),
         backgroundColor: darkGreen);
+  }
+}
+
+class ExpansionPanels extends StatelessWidget {
+  String header;
+  Widget contant;
+ExpansionPanels({required this.header ,required this.contant});
+
+  @override
+  Widget build(BuildContext context) {
+    return ExpansionWidget(
+
+        initiallyExpanded: true,
+        titleBuilder:
+            (double animationValue, _, bool isExpaned, toogleFunction) {
+          return InkWell(
+              onTap: () => toogleFunction(animated: true),
+              child: Container(
+              padding: EdgeInsets.only(right: 10),
+                color: lightGreen,
+                child: Row(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                   
+                    Transform.rotate(
+                      angle: math.pi * animationValue / 2,
+                      child: Icon(
+                        Icons.arrow_right,
+                        size: 40,
+                        color: Colors.white,
+                      ),
+                      alignment: Alignment.center,
+                    ),
+                     Expanded(
+                        child: Text(
+                      header,
+                      textAlign: TextAlign.right,
+                      style: TextStyle(color: white,fontSize: 18,fontWeight: FontWeight.bold),
+                    )),
+                  ],
+                ),
+              ));
+        },
+        content: contant);
   }
 }

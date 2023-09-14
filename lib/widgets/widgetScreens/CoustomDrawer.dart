df1c18a5bee44039a273a63bdc416ac29f44232a
triborg34 <aref.mousavi0914@gmail.com>
2023-09-14 14:56:08 +0330

    changes

--- lib/widgets/widgetScreens/CoustomDrawer.dart
+++ lib/widgets/widgetScreens/CoustomDrawer.dart
@@ -1,9 +1,10 @@
+import 'package:expansion_widget/expansion_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:seyedbox/controllers/controllers.dart';
 import 'package:seyedbox/screens/AccountScreen.dart';
 import 'package:seyedbox/utils/constracts.dart';
-import 'package:seyedbox/widgets/CollapseWidget.dart';
+import 'dart:math' as math;
 
 class CoustomDrawer extends StatelessWidget {
   const CoustomDrawer({
@@ -20,18 +21,19 @@ class CoustomDrawer extends StatelessWidget {
           shrinkWrap: true,
           children: [
             DrawerHeader(
-                child:
-                    Center(child: Image.asset("assets/images/Logo.png"))),
+                child: Center(child: Image.asset("assets/images/Logo.png"))),
             Container(
               margin: EdgeInsets.symmetric(vertical: 1),
               color: lightGreen,
               child: ListTile(
-                trailing: Icon(Icons.person,color: white,),
+                trailing: Icon(
+                  Icons.person,
+                  color: white,
+                ),
                 onTap: () {
                   Get.find<BoolControllers>().navigateIndex.value = 2;
                   Get.find<ScreensController>().body = AccountScreen();
-                  Get.find<ScreensController>().appBarTitle.value =
-                      "Account";
+                  Get.find<ScreensController>().appBarTitle.value = "Account";
 
                   Get.find<ScreensController>().update([1]);
 
@@ -43,14 +45,56 @@ class CoustomDrawer extends StatelessWidget {
                 style: ListTileStyle.list,
               ),
             ),
-            CollapeWidget(),
+            // CollapeWidget(),
+            ExpansionWidget(
+
+                initiallyExpanded: true,
+                titleBuilder:
+                    (double animationValue, _, bool isExpaned, toogleFunction) {
+                  return InkWell(
+                      onTap: () => toogleFunction(animated: true),
+                      child: Container(
+                      padding: EdgeInsets.only(right: 10),
+                        color: lightGreen,
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                           
+                            Transform.rotate(
+                              angle: math.pi * animationValue / 2,
+                              child: Icon(
+                                Icons.arrow_right,
+                                size: 40,
+                                color: Colors.white,
+                              ),
+                              alignment: Alignment.center,
+                            ),
+                             Expanded(
+                                child: Text(
+                              'فیلم',
+                              textAlign: TextAlign.right,
+                              style: TextStyle(color: white,fontSize: 18,fontWeight: FontWeight.bold),
+                            )),
+                          ],
+                        ),
+                      ));
+                },
+                content: Container(
+                  width: double.infinity,
+                  color: darkGreen,
+                  padding: EdgeInsets.all(20),
+                  child: Column(children: [
+                    
+                  ],)
+                )),
             Container(
-              
                 color: Colors.red,
                 child: ListTile(
                   style: ListTileStyle.list,
-                  trailing: Icon(Icons.exit_to_app,color: white,),
-                 
+                  trailing: Icon(
+                    Icons.exit_to_app,
+                    color: white,
+                  ),
                   title: Text(
                     "خارج شدن",
                     textAlign: TextAlign.right,
